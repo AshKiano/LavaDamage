7c0e5c51253ff2b49401bcfb46fbc39ccefb2a34
AshKiano <ash.kiano@gmail.com>
2023-07-04 21:15:20 +0200

    init

---
+++ src/main/java/com/ashkiano/lavadamage/LavaDamage.java
@@ -0,0 +1,52 @@
+package com.ashkiano.lavadamage;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+//TODO přidat na git
+//TODO přidat configurovatelnost
+//TODO udělat něco jako zčervenání obrazovky
+//TODO přidat výpis do chatu a nebo do actionbaru že je horko a jsi blízko lávy
+//TODO udelat aby ten efekt negovala permise
+//TODO udelat aby ten efekt negovalo netherite brnko
+//TODO udelat aby ten efekt negoval nějaký enchant
+public class LavaDamage extends JavaPlugin {
+    private static final int CHECK_RADIUS = 5;
+    private static final int DAMAGE_AMOUNT = 2;
+
+    @Override
+    public void onEnable() {
+        // Initialize Metrics for plugin analytics
+        Metrics metrics = new Metrics(this, 18976);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    // Check if player is in survival or adventure mode
+                    if (player.getGameMode() == GameMode.SURVIVAL || player.getGameMode() == GameMode.ADVENTURE) {
+                        if (isNearLava(player)) {
+                            player.damage(DAMAGE_AMOUNT);
+                        }
+                    }
+                }
+            }
+        }.runTaskTimer(this, 0L, 20L);  // Run every second (20 ticks = 1 second)
+    }
+
+    private boolean isNearLava(Player player) {
+        for (int x = -CHECK_RADIUS; x <= CHECK_RADIUS; x++) {
+            for (int y = -CHECK_RADIUS; y <= CHECK_RADIUS; y++) {
+                for (int z = -CHECK_RADIUS; z <= CHECK_RADIUS; z++) {
+                    if (player.getLocation().add(x, y, z).getBlock().getType() == Material.LAVA) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
