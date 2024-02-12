d418b777ec8d74ccb4a6fe65fda4049dbfd3454a
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-02-12 11:06:46 +0100

    version 1.10

--- src/main/java/com/ashkiano/lavadamage/LavaDamage.java
+++ src/main/java/com/ashkiano/lavadamage/LavaDamage.java
@@ -8,6 +8,12 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 //TODO přidat reload configu
 //TODO udělat něco jako zčervenání obrazovky
@@ -35,6 +41,8 @@ public class LavaDamage extends JavaPlugin {
         Metrics metrics = new Metrics(this, 18976);
 
         this.getLogger().info("Thank you for using the LavaDamage plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
+
+        checkForUpdates();
 
         // Run a task every second (20 ticks = 1 second) for every player online
         new BukkitRunnable() {
@@ -79,6 +87,46 @@ public class LavaDamage extends JavaPlugin {
                 player.getInventory().getChestplate() != null && player.getInventory().getChestplate().getType() == Material.NETHERITE_CHESTPLATE &&
                 player.getInventory().getLeggings() != null && player.getInventory().getLeggings().getType() == Material.NETHERITE_LEGGINGS &&
                 player.getInventory().getBoots() != null && player.getInventory().getBoots().getType() == Material.NETHERITE_BOOTS;
+    }
+
+    private void checkForUpdates() {
+        try {
+            String pluginName = this.getDescription().getName();
+            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String jsonResponse = response.toString();
+                JSONObject jsonObject = new JSONObject(jsonResponse);
+                if (jsonObject.has("error")) {
+                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                } else {
+                    String latestVersion = jsonObject.getString("latest_version");
+
+                    String currentVersion = this.getDescription().getVersion();
+                    if (currentVersion.equals(latestVersion)) {
+                        this.getLogger().info("This plugin is up to date!");
+                    } else {
+                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    }
+                }
+            } else {
+                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            }
+        } catch (Exception e) {
+            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
+        }
     }
 
 }
